8c713561203dd425e769ab8704d7a0ba9ae99a25
arkol <maxim@DESKTOP-PDRCOL0>
2025-07-07 17:05:12 +0300

    first steps

--- ZRL.py
+++ ZRL.py
@@ -1,6 +1,9 @@
 import msvcrt
 import time
+import os
 
+MAPWIDTH = 20
+MAPHEIGHT = 10
 
 class GameObject:
     def __init__(self, pos_x, pos_y, symbol):
@@ -17,7 +20,15 @@ class Player(GameObject):
         self.health = 100
         self.attack = 10
         self.defense = 5
-
+    def act(self, key):
+        if key in [b'a', b'A']:
+            self.x -= 1
+        elif key in [b'd', b'D']:
+            self.x += 1
+        if self.x < 0:
+            self.x = MAPWIDTH-1
+        elif self.x >= MAPWIDTH:
+            self.x = 0
 
 class GameMap:
     def __init__(self, width, height):
@@ -25,7 +36,7 @@ class GameMap:
         self.height = height
         self.map = [[" " for _ in range(width)] for _ in range(height)]
     
-    #добавляет стены по краям карты
+    # adds walls at map borders
     def MakeBorders (self):
         for x in range(self.width):
             self.map[0][x] = "#"
@@ -33,13 +44,26 @@ class GameMap:
         for y in range(self.height):
             self.map[y][0] = "#"
             self.map[y][self.width - 1] = "#"
+    
     def show (self):
         for row in self.map:
             print("".join(row))
 
 
 player = Player(1, 8)
-print(player.describe())
-game_map = GameMap(20, 10)
-game_map.MakeBorders()          
-game_map.show()
+
+objects_list = [player]
+
+while True:
+    game_map = GameMap(MAPWIDTH, MAPHEIGHT)
+    game_map.MakeBorders()
+    os.system('cls')
+    key = '0'
+    if msvcrt.kbhit():
+        key = msvcrt.getch()
+    time.sleep(0.02)
+    print(key)
+    print(game_map)
+    player.act(key)
+    game_map.map[player.y][player.x] = '@'
+    game_map.show()
